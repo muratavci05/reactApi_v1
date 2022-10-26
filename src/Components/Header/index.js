41149702f7cd537800ae002ada01d8d4921290a9
muratavci05 <js.muratavci@gmail.com>
2022-10-26 19:37:21 +0300

    modified header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -29,7 +29,7 @@ const Header = (props) => {
     <header>
       <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
         <a href="#/" className="d-flex align-items-center text-dark text-decoration-none">
-          <span className="fs-4">Api Tutorial</span>
+          <span className="fs-4">API Tutorial</span>
         </a>
 
         Token: {props.authState.token}
